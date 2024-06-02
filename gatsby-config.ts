5cfceb5b82d45a1061b84270f94837f1749669cc
kjirou <kjirou.web@gmail.com>
2024-06-03 00:03:23 +0900

    Fix adapter setting

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,7 +11,7 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [],
-  adapter,
+  adapter: adapter(),
 };
 
 export default config;
